b108ceb8d4df7ddb4566d40a07ece0c0dbfb1c44
cesarlongart101 <cesarlongart101@gmail.com>
2023-10-26 21:41:45 +0200

    Cargando_datos

--- ETL_PySpark.py
+++ ETL_PySpark.py
@@ -2,4 +2,13 @@ import pyspark
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import IntegerType
 
+# spark = SparkSession.builder.appName("etl_datos").getOrCreate()
+spark = SparkSession.builder.appName("etl_datos").config("spark.sql.debug.maxToStringFields", "10").getOrCreate()
+sqlcontext = SQLContext(spark)
 
+
+# path = "./Dataset_ETL_Pyspark/"
+# players_21 = spark.read.csv(path+"players_21.csv")
+# print(players_21)
+players_21 = spark.read.format("csv").option("header", "true").load("./Dataset_ETL_Pyspark/players_21.csv")
+players_21.show()
