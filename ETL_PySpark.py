55b208d14fecc1db13348352ea53f8569dae590a
cesarlongart101 <cesarlongart101@gmail.com>
2023-11-08 20:33:51 +0100

    Avance_RDD

--- ETL_PySpark.py
+++ ETL_PySpark.py
@@ -1,31 +1,40 @@
 import pyspark
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import IntegerType, StringType, StructType, StructField
+from pyspark.sql.functions import col, lit
 
-# spark = SparkSession.builder.appName("etl_datos").getOrCreate()
-spark = SparkSession.builder.appName("etl_datos").config("spark.sql.debug.maxToStringFields", "10").getOrCreate()
+spark = SparkSession.builder.appName("etl_datos").getOrCreate()
+# spark = SparkSession.builder.appName("etl_datos").config("spark.sql.debug.maxToStringFields", "10").getOrCreate()
 sqlcontext = SQLContext(spark)
-
+print("Paso 1")
 
 path = "./Dataset_ETL_Pyspark/"
 players_21 = spark.read.csv(path+"players_21.csv")
-# print(players_21)
+print("Paso 2")
+print(players_21)
+print("Paso 3")
 # players_21 = spark.read.format("csv").option("header", "true").load("./Dataset_ETL_Pyspark/players_21.csv")
-# players_21.show()
+players_21.show()
+print("Paso 4")
+
+
 
 players_21 = players_21.select('_c3', '_c16','_c33','_c34','_c35','_c36','_c37','_c38')
-# print(players_21)
+print(players_21)
+players_21.show()
+
 
 def dropfirstrow(index, iterator):
     return iter(list(iterator)[1:])
 
+
 rdd = players_21.rdd
 
-# print(rdd)
-
-dropfirstrow()
+# print(rdd.collect())
+print("Paso 5")
+# dropfirstrow()
 rdd = rdd.mapPartitionsWithIndex(dropfirstrow)
-
+print("Paso 6")
 schema = StructType([
     StructField("name",StringType(),False),
     StructField("Positition",StringType(),True),
@@ -36,7 +45,18 @@ schema = StructType([
     StructField("DEF",StringType(),False),
     StructField("PHY",StringType(),False)
 ])
-
-dataframe = SQLContext.createDataFrame (rdd,schema)
+print("Paso 7")
+dataframe = sqlcontext.createDataFrame (rdd,schema)
 dataframe.printSchema()
 print(dataframe)
+
+print("Paso 8")
+# dataframe = dataframe.dropna()
+
+# dataframe = dataframe.withColumn("PAC",col("PAC").cast(IntegerType()))
+# dataframe = dataframe.withColumn("SHO",col("SHO").cast(IntegerType()))
+# dataframe = dataframe.withColumn("PAS",col("PAS").cast(IntegerType()))
+# dataframe = dataframe.withColumn("DRI",col("DRI").cast(IntegerType()))
+# dataframe = dataframe.withColumn("DEF",col("DEF").cast(IntegerType()))
+# dataframe = dataframe.withColumn("PHY",col("PHY").cast(IntegerType()))
+
