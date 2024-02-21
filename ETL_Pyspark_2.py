92046f8f9bdd677af8dec66a8a9709b20b4a7f70
cesarlongart101 <cesarlongart101@gmail.com>
2024-02-21 22:43:37 +0100

    Carga_datos_proyecto_2

--- ETL_Pyspark_2.py
+++ ETL_Pyspark_2.py
@@ -1,6 +1,7 @@
 import findspark
 from pyspark.sql import SparkSession, SQLContext
 from pyspark import SparkContext
+from IPython.display import display 
 
 findspark.init()
 
@@ -9,8 +10,19 @@ SpSession = SparkSession \
     .appName("etl_ventas") \
     .getOrCreate()
 
-SpContext = SpSession.SparkContext
+SpContext = SQLContext(SpSession)
 
-data = SpSession.read.csv('Ventas.csv')
+data = SpSession.read.csv('./Dataset_ETL_Pyspark/Ventas.csv', header = True, sep=';')
+# data.show(truncate=False)
+# print(data)
+# print('Tipo de dato:')
+# print(type(data))
 
-data.show()
+data.printSchema()
+print(data.columns)
+print(data.dtypes)
+
+
+
+# dataframe = SpContext.createDataFrame(data)
+# display(data)
